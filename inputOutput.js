dd4902a17b7911d91a06ace2ac7a0593d5d195ac
valeriusturcanus <valeratrcn@gmail.com>
2015-03-31 13:31:15 +0100

    inputOutputSolved

--- inputOutput.js
+++ inputOutput.js
@@ -1,3 +1,9 @@
 var list = process.argv;
 var path = list[2];
 var fs = require('fs');
+// console.log(list[1]);
+// console.log('-------------------');
+// console.log(path);
+// console.log(fs);
+var str = fs.readFileSync(path);
+console.log(str.toString().split('\n').length-1 );
